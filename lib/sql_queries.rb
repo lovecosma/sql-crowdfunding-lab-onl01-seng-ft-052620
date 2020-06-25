d1de46dec41f50ce0ae4d67c26c564ce2300d2b9
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-25 01:23:10 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -31,8 +31,8 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   FROM projects
   INNER JOIN pledges
   ON pledges.project_id = projects.id
-  HAVING difference >= 0
-  GROUP BY projects.title;"
+    GROUP BY projects.title
+  HAVING difference >= 0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
