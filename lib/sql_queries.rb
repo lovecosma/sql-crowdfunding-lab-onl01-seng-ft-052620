c3da0786824384424a35a3249578f0d927718f6f
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-25 01:26:16 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -43,6 +43,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
   FROM users
   INNER JOIN pledges
   ON pledges.user_id = users.id
+  GROUP BY users.name
   SQL
 end
 
