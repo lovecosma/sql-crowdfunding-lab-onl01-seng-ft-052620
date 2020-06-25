c5bbc2b7d8a1a82ae3a261d81b3750242689d3f5
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-25 01:29:12 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -51,7 +51,7 @@ end
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   <<-SQL
   SELECT
-  projects.categories,
+  projects.category,
   SUM(pledges.amount) as amount
   FROM projects
   INNER JOIN pledges
