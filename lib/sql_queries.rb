60d04abf9b8d809f0e662e966b8c2a585a5625f3
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-25 01:44:21 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -64,7 +64,7 @@ def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_b
   <<-SQL
   SELECT
   projects.category,
-  pledges.amount
+  SUM(pledges.amount)
   FROM projects
   INNER JOIN pledges
   ON projects.id = pledges.project_id
