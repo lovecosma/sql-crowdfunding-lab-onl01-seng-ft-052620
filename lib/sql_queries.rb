4da1ca3c9cc74569e65bf39703d01e78c6ae1ee7
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-25 01:11:38 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,7 +27,7 @@ end
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
  "SELECT
   projects.title as title,
-  SUM(pledges.amount)-projects.funding_goal
+  SUM(pledges.amount)-projects.funding_goal AS difference
   FROM projects
   INNER JOIN pledges
   ON pledges.project_id = projects.id
