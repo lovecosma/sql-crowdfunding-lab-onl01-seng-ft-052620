8417feaed7b54aee3533a125bd465910998f86c3
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-25 01:43:09 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -55,7 +55,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   pledges.amount
   FROM projects
   INNER JOIN pledges
-  ON projects.id = pledges.project_ids;
+  ON projects.id = pledges.project_id;
   WHERE projects.category = "music"
   SQL
 end
