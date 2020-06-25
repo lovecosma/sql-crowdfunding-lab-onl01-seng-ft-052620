c3c9fde5ea878ba14c1819e7026c89eb8735a018
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-25 01:05:53 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,11 +27,12 @@ end
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
  "SELECT
   projects.title as title,
-  SUM(pledges.amount)
+  SUM(pledges.amount) - projects.funding_goal
   FROM projects
   INNER JOIN pledges
   ON pledges.project_id = projects.id
   GROUP BY projects.title;"
+  WHERE
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
